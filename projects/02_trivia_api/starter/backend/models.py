fd3834360be0bf5ea7122be7986d3a021edb6b58
sultan12100 <sultan12100@hotmail.com>
2020-06-13 16:56:31 +0300

    edit code

--- projects/02_trivia_api/starter/backend/models.py
+++ projects/02_trivia_api/starter/backend/models.py
@@ -5,7 +5,7 @@ import json
 
 database_name = "trivia"
 database_path = "postgres://{}/{}".format(
-     '@localhost:5432', database_name)
+     'localhost:5432', database_name)
 
 db = SQLAlchemy()
 
