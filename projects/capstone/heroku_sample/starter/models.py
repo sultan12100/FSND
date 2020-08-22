cdccfc8acb538574831cc40a027418a532214dff
sultan12100 <sultan12100@hotmail.com>
2020-08-22 19:23:11 +0300

    change

--- projects/capstone/heroku_sample/starter/models.py
+++ projects/capstone/heroku_sample/starter/models.py
@@ -16,7 +16,7 @@ def setup_db(app, database_path=database_path):
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
     db.init_app(app)
-    db.create_all()
+    #db.create_all()
 
 
 '''
