9ada4e032a8d06fe93304e8b1469a2d97422d638
sultan12100 <sultan12100@hotmail.com>
2020-08-22 05:20:05 +0300

    deploy2

--- projects/capstone/heroku_sample/starter/app.py
+++ projects/capstone/heroku_sample/starter/app.py
@@ -20,6 +20,10 @@ def create_app(test_config=None):
     def be_cool():
         return "Be cool, man, be coooool! You're almost a FSND grad!"
 
+    @app.route('/dbg')
+    def dbg():
+        return os.environ['DATABASE_URL']
+
     return app
 
 app = create_app()
